0bad4d641362015e5663de2df615c8225a0ed919
edeeeen <lsmall@sleepycat.me>
2026-02-05 12:02:04 -0500

    Implement TextSizeAdapter, make it work with MainActivitiy

--- app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
@@ -1,25 +1,56 @@
 package edu.temple.inclassuiacvitivity
 
+import android.content.Context
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.TextView
 
-class TextSizeAdapter : BaseAdapter() {
+class TextSizeAdapter(private val context: Context, private val objects: Array<Int> ) : BaseAdapter() {
 
     override fun getCount(): Int {
-        TODO("Not yet implemented")
+        return objects.size;
     }
 
     override fun getItem(position: Int): Any {
-        TODO("Not yet implemented")
+        return objects[position];
     }
 
     override fun getItemId(position: Int): Long {
-        TODO("Not yet implemented")
+        return position.toLong();
     }
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        TODO("Not yet implemented")
+        val textView: TextView;
+
+        if(convertView != null) {
+            textView = convertView as TextView
+        } else {
+            textView = TextView(context);
+        }
+
+
+        textView.text = objects[position].toString()
+        textView.textSize = 22.toFloat();
+        textView.setPadding(20, 20, 20, 20);
+
+        return textView
+
+    }
+
+    override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup?): View? {
+        val textView: TextView;
+
+        if(convertView != null) {
+            textView = convertView as TextView
+        } else {
+            textView = TextView(context);
+        }
+        textView.text = objects[position].toString()
+        textView.textSize = objects[position].toFloat()
+        textView.setPadding(20, 20, 20, 20);
+
+        return textView
     }
 
 }
